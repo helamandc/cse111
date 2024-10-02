e453da2b2ecfa12b5b02735ef7a284ba8ae24f49
helamandc <manlehcastillo@gmail.com>
2024-10-03 09:15:56 +1000

    Improved Team Activity

--- week3/test_names.py
+++ week3/test_names.py
@@ -3,16 +3,21 @@ import pytest
 
 def test_make_full_name():
     assert make_full_name("Manleh","Castillo") == "Castillo;Manleh"
+    assert make_full_name("Eva","Castillo-Guatno") == "Castillo-Guatno;Eva"
+    assert make_full_name("J","Li") == "Li;J"
+    assert make_full_name("","") == ";"
 
 def test_extract_family_name():
-    full_name = make_full_name("Manleh","Castillo")
-    family_name = extract_family_name(full_name)
-    assert family_name == "Castillo"
+    assert extract_family_name("Castillo;Manleh") == "Castillo"
+    assert extract_family_name("Castillo-Guatno;Eva") == "Castillo-Guatno"
+    assert extract_family_name("Li;J") == "Li"
+    assert extract_family_name(";") == ""
 
 def test_extract_given_name():
-    full_name = make_full_name("Manleh","Castillo")
-    given_name = extract_given_name(full_name)
-    assert given_name == "Manleh"
+    assert extract_given_name("Castillo;Manleh") == "Manleh"
+    assert extract_given_name("Castillo-Guatno;Eva") == "Eva"
+    assert extract_given_name("Li;J") == "J"
+    assert extract_given_name(";") == ""
 
 # Call the main function that is part of pytest so that the
 # computer will execute the test functions in this file.
