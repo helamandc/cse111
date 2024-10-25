c376e2f7ff34bad0e00b43909a84f2c3ea7feae5
helamandc <manlehcastillo@gmail.com>
2024-10-26 10:45:00 +1100

    Week 6 Milestone personal project

---
+++ password.py
@@ -0,0 +1,124 @@
+"""
+Title: Basic Password Generator
+Description: It is very important for each person to change his/her password on a regular basis to prevent being compromised. Sometimes, thinking about what password to use is time consuming because you can't just re use your old passwords. In order to solve this problem, this program will help generate basic password for users.
+Author: Helaman Del Castillo
+Date: October 26, 2024
+
+"""
+from datetime import datetime, timedelta
+import csv
+import random
+
+
+
+def main():
+    print("Basic password generator")
+    print("This program will help you generate a password that is easy to remember.\n")
+
+    nick_name = input("Input your nick name: ")
+    birth_date = input("Input your birthdate (mmddyy): ")
+
+    new_password = generate_password(nick_name, birth_date)
+    
+    print(f"\nYour new password is: {new_password}\n")
+
+    date_today = datetime.now()
+
+    password_change_date = password_change(date_today)
+
+    print(f"Note: You need to change your password by {password_change_date:%B %d, %Y}")
+
+    password_storage_file = password_storage(new_password)
+
+    print(f"Please refer to this file in your local machine for your password storage: {password_storage_file}")
+
+def generate_password(name, date):
+    """Generates password that follows the following criteria and then returns a string:
+    at least 1 uppercase
+    at least 1 special character
+    at least 1 number
+    at least 8 characters
+    no spaces in between
+
+    Parameters
+        name: the nick name of the user
+        date: the birthdate of the user
+
+    Return: a string that is composed of the password criteria mentioned above
+    """
+    special_char_list = ["@","$","#","%","?","!"]
+    update_char_dict = {
+    #leet speak equivalents
+    "a": "@",
+    "b": "8",
+    "e": "3",
+    "g": "6",
+    "i": "1",
+    "o": "0",
+    "s": "$",
+    "t": "+"
+    }
+
+    new_password_list = []
+
+    for letter in name:
+        new_password_list.append(letter)
+    
+    for num in date:
+        new_password_list.append(num)
+
+    count = len(new_password_list)
+
+    for i in range(count):
+        if new_password_list[i] in update_char_dict:
+            new_password_list[i] = update_char_dict[new_password_list[i]]
+    
+    #find where the first digit of the birthday is and insert a special character
+    special_char_insert = random.choice(special_char_list)
+    #6th to the last character in the list
+    first_digit = new_password_list[-6]
+    index = new_password_list.index(first_digit)
+    new_password_list.insert(index, special_char_insert)
+    
+    new_password_generate = "".join(new_password_list)
+
+    return new_password_generate
+
+
+def password_change(current_day):
+    """Provides the next day the user needs to change his password which is after 3 months
+
+    Parameters
+        current_day: the current date
+
+    Return: a date in this format e.g. January 1, 2025
+    """
+    change_password_date = current_day + timedelta(days=90)
+
+    return change_password_date
+
+def password_storage(new_password):
+    """Appends the newly generated password into the password storage file
+
+    Parameters
+        new_password: the new password generated
+
+    Return: the name of the password storage file
+    """
+    pass
+
+# If this file is executed like this:
+# > python password.py
+# then call the main function. However, if this file is simply
+# imported (e.g. into a test file), then skip the call to main.
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
